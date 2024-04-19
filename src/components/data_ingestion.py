88a6b4fdf0d764e82704c40191a3f3e4434c2255
Ritwik <ritwik1080@gmail.com>
2024-04-19 18:35:48 +0530

    artifacts/preprocessor.pkl, requirements.txt, components/data_ingestion.py, data_transformation.py, src/utils.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -19,10 +19,14 @@ from src.exception import CustomException
 from src.logger import logging 
 import pandas as pd 
 
+
 from sklearn.model_selection import train_test_split 
 from dataclasses import dataclass 
 # 'dataclass' a way to create classes specifically for holding data. They simplify the process by using a decorator and type 
 # hints to automatically generate common methods like __init__ and __repr__.
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass # dont need __init__ or need to define the class variable
 class DataIngestionConfig:
@@ -59,4 +63,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
