15379eea9a7fa21316267673dc40aff0634dc9cd
Ritwik <ritwik1080@gmail.com>
2024-04-19 23:57:44 +0530

    artifacts/model.pkl, components/data_ingestion.py, model_trainer.py, src/utils.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -28,6 +28,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass # dont need __init__ or need to define the class variable
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')  # All the outputs will be stored in the artifacts folder 
@@ -67,3 +70,6 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
